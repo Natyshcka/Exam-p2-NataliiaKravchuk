574f3acbf187e4d0a780aa23b46ca83619edec37
Nataliia Kravchuk <kr.nata5@gmail.com>
2023-07-04 00:34:34 +0300

    comt

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,131 +1,221 @@
-import user from "../fixtures/user.json"
-
 describe('API Tests', () => {
-  
+  let accessToken;
+
+  before(() => {
+    cy.request('POST', 'http://localhost:3000/register', {
+      email: 'olivier@mail.com',
+      password: 'bestPassw0rd',
+      firstname: 'Olivier',
+      lastname: 'Monge',
+      age: 32
+    })
+      .its('body.accessToken')
+      .should('exist')
+      .then((token) => {
+        accessToken = token;
+      });
+  });
+
+  it('should login with the registered user', () => {
+    cy.request('POST', 'http://localhost:3000/login', {
+      email: 'olivier@mail.com',
+      password: 'bestPassw0rd'
+    })
+      .its('body.accessToken')
+      .should('exist')
+      .then((token) => {
+        accessToken = token;
+      });
+  });
+
+  it('should access a protected resource with a valid token', () => {
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:3000/protected',
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+      .its('status')
+      .should('eq', 200);
+  });
+
+  it('should create a post (with an access token)', () => {
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3000/posts',
+      body: {
+        title: 'New Post',
+        body: 'This is a new post'
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+      .its('status')
+      .should('eq', 201);
+  });
 
   it('should get all posts', () => {
     cy.request('GET', 'http://localhost:3000/posts')
       .its('status')
-      .should('eq', 200)
+      .should('eq', 200);
+  });
+});
 
-    cy.request('GET', 'http://localhost:3000/posts')
-      .its('headers')
-      .its('content-type')
-      .should('include', 'application/json')
+it('should login with the registered user', () => {
+  cy.request('POST', 'http://localhost:3000/login', {
+    email: user.email,
+    password: user.password
   })
+    .its('status')
+    .should('eq', 200)
+    .its('body.accessToken')
+    .should('exist')
+    .then((token) => {
+      accessToken = token;
+    });
+});
 
-  it('should get only first 10 posts', () => {
-    cy.request('GET', 'http://localhost:3000/posts?_limit=10')
-      .its('status')
-      .should('eq', 200)
-
-    cy.request('GET', 'http://localhost:3000/posts?_limit=10')
-      .its('body')
-      .should('have.length', 10)
+it('should access a protected resource with a valid token', () => {
+  cy.request({
+    method: 'GET',
+    url: 'http://localhost:3000/protected',
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
   })
+    .its('status')
+    .should('eq', 200)
+    .its('body')
+    .should('deep.include', { email: user.email });
+});
 
-  it('should get posts with specific ids', () => {
-    cy.request('GET', 'http://localhost:3000/posts?id=55&id=60')
-      .its('status')
-      .should('eq', 200)
+it('should get all posts', () => {
+  cy.request('GET', 'http://localhost:3000/posts')
+    .its('status')
+    .should('eq', 200)
+    .its('headers')
+    .its('content-type')
+    .should('include', 'application/json');
+});
 
-    cy.request('GET', 'http://localhost:3000/posts?id=55&id=60')
-      .its('body')
-      .should('deep.include', [{ id: 55 }, { id: 60 }])
+it('should get only the first 10 posts', () => {
+  cy.request('GET', 'http://localhost:3000/posts?_limit=10')
+    .its('status')
+    .should('eq', 200)
+    .its('body')
+    .should('have.length', 10);
+});
+
+it('should get posts with specific IDs', () => {
+  cy.request('GET', 'http://localhost:3000/posts?id=55&id=60')
+    .its('status')
+    .should('eq', 200)
+    .its('body')
+    .should('deep.include', [{ id: 55 }, { id: 60 }]);
+});
+
+it('should create a post (without an access token)', () => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3000/posts',
+    failOnStatusCode: false
   })
+    .its('status')
+    .should('eq', 401);
+});
 
-  it('should create a post (without access token)', () => {
-    cy.request({
-      method: 'POST',
-      url: 'http://localhost:3000/posts',
-      failOnStatusCode: false
-    })
-      .its('status')
-      .should('eq', 401)
+it('should create a post (with an access token)', () => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3000/posts',
+    body: {
+      title: 'New Post',
+      body: 'This is a new post'
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
   })
+    .its('status')
+    .should('eq', 201);
+});
 
-  it('should create a post (with access token)', () => {
-    cy.get('@user').then((user) => {
-      cy.request('POST', 'http://localhost:3000/posts', {
-        title: 'New Post',
-        body: 'This is a new post'
-      }, {
-        headers: {
-          Authorization: `Bearer ${user.token}`
+it('should create a post entity', () => {
+  cy.request('POST', 'http://localhost:3000/posts', {
+    title: 'New Post',
+    body: 'This is a new post'
+  })
+    .its('status')
+    .should('eq', 201);
+});
+
+it('should update a non-existing entity', () => {
+  cy.request({
+    method: 'PUT',
+    url: 'http://localhost:3000/posts/999',
+    failOnStatusCode: false
+  })
+    .its('status')
+    .should('eq', 404);
+});
+
+it('should create and update a post entity', () => {
+  cy.request('POST', 'http://localhost:3000/posts', {
+    title: 'New Post',
+    body: 'This is a new post'
+  })
+    .then((response) => {
+      const postId = response.body.id;
+
+      cy.request({
+        method: 'PUT',
+        url: `http://localhost:3000/posts/${postId}`,
+        body: {
+          title: 'Updated Post',
+          body: 'This post has been updated'
         }
       })
         .its('status')
-        .should('eq', 201)
-    })
+        .should('eq', 200);
+    });
+});
+
+it('should delete a non-existing post entity', () => {
+  cy.request({
+    method: 'DELETE',
+    url: 'http://localhost:3000/posts/999',
+    failOnStatusCode: false
   })
+    .its('status')
+    .should('eq', 404);
+});
 
-  it('should create a post entity', () => {
-    cy.request('POST', 'http://localhost:3000/posts', {
-      title: 'New Post',
-      body: 'This is a new post'
-    })
-      .its('status')
-      .should('eq', 201)
+it('should create, update, and delete a post entity', () => {
+  cy.request('POST', 'http://localhost:3000/posts', {
+    title: 'New Post',
+    body: 'This is a new post'
   })
+    .then((response) => {
+      const postId = response.body.id;
 
-  it('should update a non-existing entity', () => {
-    cy.request({
-      method: 'PUT',
-      url: 'http://localhost:3000/posts/999',
-      failOnStatusCode: false
-    })
-      .its('status')
-      .should('eq', 404)
-  })
-
-  it('should create and update a post entity', () => {
-    cy.request('POST', 'http://localhost:3000/posts', {
-      title: 'New Post',
-      body: 'This is a new post'
-    })
-      .then((response) => {
-        const postId = response.body.id
-
-        cy.request('PUT', `http://localhost:3000/posts/${postId}`, {
+      cy.request({
+        method: 'PUT',
+        url: `http://localhost:3000/posts/${postId}`,
+        body: {
           title: 'Updated Post',
           body: 'This post has been updated'
-        })
-          .its('status')
-          .should('eq', 200)
+        }
       })
-  })
+        .its('status')
+        .should('eq', 200);
 
-  it('should delete a non-existing post entity', () => {
-    cy.request({
-      method: 'DELETE',
-      url: 'http://localhost:3000/posts/999',
-      failOnStatusCode: false
-    })
-      .its('status')
-      .should('eq', 404)
-  })
-
-  it('should create, update, and delete a post entity', () => {
-    cy.request('POST', 'http://localhost:3000/posts', {
-      title: 'New Post',
-      body: 'This is a new post'
-    })
-      .then((response) => {
-        const postId = response.body.id
-
-        cy.request('PUT', `http://localhost:3000/posts/${postId}`, {
-          title: 'Updated Post',
-          body: 'This post has been updated'
-        })
-          .its('status')
-          .should('eq', 200)
-
-        cy.request('DELETE', `http://localhost:3000/posts/${postId}`)
-          .its('status')
-          .should('eq', 200)
+      cy.request({
+        method: 'DELETE',
+        url: `http://localhost:3000/posts/${postId}`
       })
-  })
-})
-
-
-
+        .its('status')
+        .should('eq', 200);
+    });
+});
